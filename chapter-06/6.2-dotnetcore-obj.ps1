97b119337c526db8aa9f7aa5ed287f4a2f24a494
Ram Iyer <ramakrishnan.rkology@gmail.com>
2018-07-12 10:33:53 +0530

    Just a simple cleanup

--- chapter-06/6.2-dotnetcore-obj.ps1
+++ chapter-06/6.2-dotnetcore-obj.ps1
@@ -2,14 +2,14 @@ class Person {
     [string]$Name
     [Int32]$Age
     [int32]$Salary
-    
+
     Person () {}
-    
+
     Person ([string]$Name, [int32]$Age) {
         $this.Name = $Name
         $this.Age = $Age
     }
-    
+
     [int32] sal ([int32]$Salary, [int32]$Comm) {
         return $Salary * $Comm
     }
