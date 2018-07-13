e24c418a8b3eca77fa82025d243bda74ebe7480d
Ram Iyer <ramakrishnan.rkology@gmail.com>
2018-07-13 12:11:59 +0530

    Clean up the code for SampleAzureRmVM

--- chapter-19/SampleAzureRmVM.ps1
+++ chapter-19/SampleAzureRmVM.ps1
@@ -1,54 +1,48 @@
 Param(
-# Variables for common values
-[string]$resourceGroup = "packtpub-rg",
-[string]$storageAcctName ="packtstorage",
-[string]$location = "East US",
-[string]$vnetName="packtpub-vnet",
-[string]$NicName="packtpub-vm-nic",
-[string]$diskname="packtUbuntu",
-[string]$vmName = "packtpub-ubuntu"
+    # Variables for common values
+    [string]$ResourceGroup = "packtpub-rg",
+    [string]$StorageAcctName = "packtstorage",
+    [string]$Location = "East US",
+    [string]$VNetName = "packtpub-vnet",
+    [string]$NicName = "packtpub-vm-nic",
+    [string]$DiskName = "packtUbuntu",
+    [string]$VmName = "packtpub-ubuntu"
 )
-         
 
 #Check for open session
 Connect-AzureRmAccount
 
 # Create user object
-$cred = Get-Credential -Message "Enter username and password of administrator crendentials"
+$Cred = Get-Credential -Message "Enter username and password of administrator crendentials"
 
 # Create a resource group
-New-AzureRmResourceGroup -Name $resourceGroup -Location $location
+New-AzureRmResourceGroup -Name $ResourceGroup -Location $Location
 
 #Create a storage account
-New-AzureRmStorageAccount -Name $storageAcctName -ResourceGroupName $resourceGroup -Type Standard_LRS -Location $location                                                                                    
-
+New-AzureRmStorageAccount -Name $StorageAcctName -ResourceGroupName $ResourceGroup -Type Standard_LRS -Location $Location
 
 # Create a subnet configuration
-$subnetCfg = New-AzureRmVirtualNetworkSubnetConfig -Name mySubnet -AddressPrefix 10.0.1.0/24
+$SubnetCfg = New-AzureRmVirtualNetworkSubnetConfig -Name mySubnet -AddressPrefix 10.0.1.0/24
 
 # Create a virtual network
-$vnet=New-AzureRmVirtualNetwork -Name $vnetName -ResourceGroupName $resourceGroup -Location $location -AddressPrefix 10.0.0.0/16 -Subnet $subnetCfg
+$VNet = New-AzureRmVirtualNetwork -Name $VNetName -ResourceGroupName $ResourceGroup -Location $Location -AddressPrefix 10.0.0.0/16 -Subnet $SubnetCfg
 
 # Create a public IP address and specify a DNS name
-$IP=New-AzureRmPublicIpAddress -Name $nicname -ResourceGroupName $resourceGroup -Location $location -AllocationMethod Dynamic                                                                                
-
+$IP = New-AzureRmPublicIpAddress -Name $NicName -ResourceGroupName $ResourceGroup -Location $Location -AllocationMethod Dynamic
 
 # Create a virtual network card and associate with public IP address and NSG
-$nic=New-AzureRmNetworkInterface -Name $nicname -ResourceGroupName $resourceGroup -Location $location -SubnetId $vnet.Subnets[0].Id -PublicIpAddressId $IP.Id                                                
-
+$Nic = New-AzureRmNetworkInterface -Name $NicName -ResourceGroupName $ResourceGroup -Location $Location -SubnetId $VNet.Subnets[0].Id -PublicIpAddressId $IP.Id
 
 #Create a blob storage
-$storageAccount=Get-AzureRmStorageAccount -ResourceGroupName $resourceGroup -Name $storageAcctName                
-$osdiskuri=$storageAccount.PrimaryEndpoints.Blob.ToString()+'vhds/'+$diskname+".vhd" 
+$StorageAccount = Get-AzureRmStorageAccount -ResourceGroupName $ResourceGroup -Name $StorageAcctName
+$OSDiskUri = $StorageAccount.PrimaryEndpoints.Blob.ToString()+'vhds/'+$DiskName+".vhd"
 
 # Create a virtual machine configuration
-$vm =New-AzureRmVMConfig -VMName $vmname -VMSize "Basic_A1"
-$vm=Set-AzureRmVMOperatingSystem -VM $vm -Linux -ComputerName $vmname -Credential $cred
-$vm=Set-AzureRmVMSourceImage -VM $vm -PublisherName 'Canonical' -Offer 'UbuntuServer' -Skus '18.04-LTS' -Version 'latest'
-$vm=Add-AzureRmVMNetworkInterface -VM $vm -id $nic.Id
-$vm=Set-AzureRmVMOSDisk -VM $vm -Name $diskname -VhdUri $osdiskuri -CreateOption fromImage 
-
-       
+$Vm = New-AzureRmVMConfig -VMName $VmName -VMSize "Basic_A1"
+$Vm = Set-AzureRmVMOperatingSystem -VM $Vm -Linux -ComputerName $VmName -Credential $Cred
+$Vm = Set-AzureRmVMSourceImage -VM $Vm -PublisherName 'Canonical' -Offer 'UbuntuServer' -Skus '18.04-LTS' -Version 'latest'
+$Vm = Add-AzureRmVMNetworkInterface -VM $Vm -id $Nic.Id
+$Vm = Set-AzureRmVMOSDisk -VM $Vm -Name $DiskName -VhdUri $OSDiskUri -CreateOption FromImage
 
 # Create a virtual machine
-New-AzureRmVM -ResourceGroupName $resourceGroup -Location $location -VM $vm  
+New-AzureRmVM -ResourceGroupName $ResourceGroup -Location $Location -VM $Vm
