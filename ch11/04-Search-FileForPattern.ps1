8e058b148ef5f2c2b2d07ee7cba7bb46697edee2
Ram Iyer <ramakrishnan.rkology@gmail.com>
2018-09-04 13:45:29 +0530

    Readability fix for 11.4

--- ch11/04-Search-FileForPattern.ps1
+++ ch11/04-Search-FileForPattern.ps1
@@ -9,19 +9,22 @@ Select-String '```powershell' $HOME/Documents/code/github/powershell/cheatsheets
 } | Out-File $ReportPath -Append
 
 <# Verbose version:
-Select-String -Pattern '```powershell' -Path $HOME/Documents/code/github/powershell/cheatsheets/*.md | ForEach-Object -Process {
-    $PSItem = $PSItem -split ':'
-    Write-Output "File Name: $($PSItem[0])"
-    Write-Output "Line number: $($PSItem[1])"
-    Write-Output "Pattern: $($PSItem[2])`n"
-}
+Select-String -Pattern '```powershell' -Path $HOME/Documents/code/github/powershell/cheatsheets/*.md
 #>
 
 "Here is a contextual report of all the PowerShell code blocks:" | Out-File $ReportPath -Append
-Select-String '```powershell' $HOME/Documents/code/github/powershell/cheatsheets/*.md -Context 2, 2 | Select-Object Path, LineNumber, @{ Name = "Before"; Expression = { $PsItem.Context.PreContext -join "`n" } }, @{ Name = "After"; Expression = { $PsItem.Context.PostContext -join "`n" } } | Format-List | Out-File $ReportPath -Append
+Select-String '```powershell' $HOME/Documents/code/github/powershell/cheatsheets/*.md -Context 2, 2 |
+    Select-Object Path, LineNumber, @{ Name = "Before"; Expression = { $PsItem.Context.PreContext -join "`n" } }, @{ Name = "After"; Expression = { $PsItem.Context.PostContext -join "`n" } } |
+        Format-List | Out-File $ReportPath -Append
 
 "Here are the files that contain the word, 'command' in them, except the cheatsheet for the first chapter:" | Out-File $ReportPath -Append
-Select-String 'command' $HOME/Documents/code/github/powershell/cheatsheets/*.md -Exclude '*chapter-01.md' | Group-Object Path | Select-Object Name, Count | Format-Table -AutoSize | Out-File $ReportPath -Append
+Select-String 'command' $HOME/Documents/code/github/powershell/cheatsheets/*.md -Exclude '*chapter-01.md' |
+    Group-Object Path |
+        Select-Object Name, Count |
+            Format-Table -AutoSize | Out-File $ReportPath -Append
 
 "Here are the files that contain the word, 'PowerShell' in them." | Out-File $ReportPath -Append
-Select-String -CaseSensitive 'PowerShell' $HOME/Documents/code/github/powershell/cheatsheets/*.md -Exclude '*chapter-01.md' | Select-Object Path -Unique | Format-Table -AutoSize | Out-File $ReportPath -Append
+Select-String -CaseSensitive 'PowerShell' $HOME/Documents/code/github/powershell/cheatsheets/*.md -Exclude '*chapter-01.md' | 
+    Select-Object Path -Unique | 
+        Format-Table -AutoSize | 
+            Out-File $ReportPath -Append
