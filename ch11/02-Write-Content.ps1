dc3698e2f408e71795e51bb12fd9202c8e954157
Ram Iyer <ramakrishnan.rkology@gmail.com>
2018-09-10 11:45:55 +0530

    Minor edit

--- ch11/02-Write-Content.ps1
+++ ch11/02-Write-Content.ps1
@@ -1,13 +1,12 @@
 Get-ChildItem $HOME/random/dir-04 | Out-File $HOME/random/file-list.txt
 
 Get-ChildItem $HOME/random/dir-05 2>&1 | Out-File $HOME/random/error.txt
-# Alternate, using only operators: 
 
 Write-Host "Listing the contents of ~/random/dir-03 and appending the list to file-list.txt." 6>&1 | Out-File $HOME/random/message.txt
 
 Get-ChildItem $HOME/random/dir-03 | Tee-Object $HOME/random/file-list.txt -Append
 
-<# Alternates, using only operators:
+<# Alternatives, using only operators:
 Get-ChildItem $HOME/random/dir-04 > $HOME/random/file-list.txt
 
 Get-ChildItem $HOME/random/dir-05 2>&1 > $HOME/random/error.txt
