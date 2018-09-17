f1c8dd6f60da03cfe0617ecb62a6f85e81769dba
Ram Iyer <ramakrishnan.rkology@gmail.com>
2018-09-17 12:51:10 +0530

    Add code for chapter 12

---
+++ ch12/06-Start-Count.ps1
@@ -0,0 +1,11 @@
+function Start-Count {
+    param ($TotalTime = 25)
+
+    $CurrentTime = 0
+
+    while ($CurrentTime -le $TotalTime) {
+        Write-Progress -Activity "Counting to $TotalTime" -Status "Elapsed time: $CurrentTime seconds" -PercentComplete ($CurrentTime / $TotalTime * 100)
+        Start-Sleep 1
+        $CurrentTime++
+    }
+}
