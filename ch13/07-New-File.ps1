9d15e2c98a3fde4e3a8cac38c80d4b2a757c0159
Ram Iyer <ramakrishnan.rkology@gmail.com>
2018-10-01 02:12:44 +0530

    Pipeline input: not yet

--- ch13/07-New-File.ps1
+++ ch13/07-New-File.ps1
@@ -5,7 +5,7 @@ function New-File {
     )]
     param (
         # The path to the file (or the name)
-        [Parameter(Mandatory=$true, Position=0, ValueFromPipeline=$true)]
+        [Parameter(Mandatory=$true, Position=0)]
         [string[]]
         $Path
     )
