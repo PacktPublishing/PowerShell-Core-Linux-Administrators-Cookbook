f993de8af6f9779b9e8843a037756e3e23863dcd
Ram Iyer <ramakrishnan.rkology@gmail.com>
2018-10-06 18:55:40 +0530

    Add 13.10

---
+++ ch13/10-New-File.ps1
@@ -0,0 +1,54 @@
+function Main {
+    New-FileName -Count 10 | New-File
+}
+
+function New-FileName {
+    param (
+        # The name of the file
+        [Parameter(Mandatory=$false)]
+        [string]
+        $Prefix='File',
+
+        # The number of files to be generated
+        [Parameter(Mandatory=$false, Position=1)]
+        [int]
+        $Count=1
+    )
+
+    begin {
+        $InitCount = 1
+    }
+
+    process {
+        while ($InitCount -le $Count) {
+            Write-Output $Prefix$InitCount
+            $InitCount++
+        }
+    }
+}
+
+function New-File {
+    [CmdletBinding(
+        ConfirmImpact='High',
+        SupportsShouldProcess=$true
+    )]
+    param (
+        # The path to the file (or the name)
+        [Parameter(Mandatory=$true, Position=0, ValueFromPipeline)]
+        [string[]]
+        $Path
+    )
+    begin {
+        Write-Host "$(Get-Date)"
+    }
+    process {
+        foreach ($Item in $Path) {
+            if ($PSCmdlet.ShouldProcess("$PsScriptRoot", "Create $Path")) {
+                New-Item -Path $Path -ItemType File
+            }
+        }
+    }
+    end {}
+}
+
+. Main
