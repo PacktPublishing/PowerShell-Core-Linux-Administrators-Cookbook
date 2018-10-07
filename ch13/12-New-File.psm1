95a88721b94bda065c92a5cb15763ac14048a4e6
Ram Iyer <ramakrishnan.rkology@gmail.com>
2018-10-07 11:33:57 +0530

    Add 13.12

---
+++ ch13/12-New-File.psm1
@@ -0,0 +1,71 @@
+function New-FileName {
+    param (
+        # The name of the file
+        [Parameter(Mandatory=$false)]
+        [string]
+        $Prefix='File',
+
+        # The number of files to be generated
+        [Parameter(Mandatory=$false, Position=1)]
+        [int]
+        $Count=1
+    )
+
+    begin {
+        $InitCount = 1
+    }
+
+    process {
+        while ($InitCount -le $Count) {
+            Write-Output $Prefix$InitCount
+            $InitCount++
+        }
+    }
+}
+
+function New-File {
+    [CmdletBinding(
+        SupportsShouldProcess=$true,
+        DefaultParameterSetName='File'
+    )]
+    param (
+        # The path to the file (or the name)
+        [Parameter(Mandatory=$true, Position=0, ValueFromPipeline, ParameterSetName='File')]
+        [string[]]
+        $Path,
+
+        # Whether files need to be generated
+        [Parameter(Mandatory=$true, ParameterSetName='Generate')]
+        [switch]
+        $Generate,
+
+        # Number of files to be generated
+        [Parameter(Mandatory=$false, ParameterSetName='Generate')]
+        [int]
+        $Count=1
+    )
+    begin {
+        Write-Host "$(Get-Date)"
+    }
+    process {
+        if ($Generate) {
+            $Path = New-FileName -Count $Count
+        }
+        foreach ($Item in $Path) {
+            if ($PSCmdlet.ShouldProcess("$PsScriptRoot", "Create $Path")) {
+                New-Item -Path $Path -ItemType File
+            }
+        }
+    }
+    end {}
+}
+
+Export-ModuleMember New-File
+
+$MyInvocation.MyCommand.ScriptBlock.Module.OnRemove = {
+    Write-Output "Module cleaned up."
+}
+
+Register-EngineEvent PowerShell.Exiting {
+    Remove-Module '12-New-File'
+}
