57d40b1f9e629798a2bbfd7e5e8ff41708b92a05
Ram Iyer <ramakrishnan.rkology@gmail.com>
2018-10-08 01:35:01 +0530

    Add 14.1 and 14.2

---
+++ ch14/02-New-File.ps1
@@ -0,0 +1,23 @@
+function New-File {
+    [CmdletBinding()]
+    param (
+        # The path to the file (or the name)
+        [Parameter(Mandatory=$true, Position=0)]
+        [string[]]
+        $Path
+    )
+    begin {
+        Write-Debug "Entered the begin block."
+        Write-Host "$(Get-Date)"
+    }
+    process {
+        Write-Debug "Entered the process block."
+        foreach ($Item in $Path) {
+            Write-Debug "Iterating for item, $Item."
+            New-Item -Path $Path -ItemType File
+        }
+    }
+    end {
+        Write-Debug "Entered the end block."
+    }
+}
