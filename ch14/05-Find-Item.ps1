4c8990dd531aebe7af73fb48b534ad6edcf51647
Ram Iyer <ramakrishnan.rkology@gmail.com>
2018-10-24 13:13:17 +0530

    Fix script for error handling

--- ch14/05-Find-Item.ps1
+++ ch14/05-Find-Item.ps1
@@ -1,7 +1,17 @@
+ping ghostname
+
+if (!$?) {
+    Write-Host "PowerShell seems to have encountered an error while running the last command."
+}
+
+Write-Host "The exit code for the last operation is $LASTEXITCODE."
+
+Remove-Variable LASTEXITCODE
+
 Get-Item $HOME/random/randompackage.zip -ErrorAction SilentlyContinue
 
 if (!$?) {
-    Write-Host "PowerShell seems to have encountered an error."
+    Write-Host "PowerShell seems to have encountered an error while running the last command."
 }
 
 Write-Host "The exit code for the last operation is $LASTEXITCODE."
