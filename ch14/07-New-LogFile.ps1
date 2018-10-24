4196d738ab50855a8ac762852f13390a43d97564
Ram Iyer <ramakrishnan.rkology@gmail.com>
2018-10-24 17:13:47 +0530

    Script to create log file with error handling

---
+++ ch14/07-New-LogFile.ps1
@@ -0,0 +1,24 @@
+function New-LogFile {
+    param (
+        # The path to the log file
+        [Parameter(Mandatory=$false)]
+        [string]
+        $Path = "$HOME/random",
+
+        # The name of the log file
+        [Parameter(Mandatory=$false)]
+        [string]
+        $Name = 'MyLog.log'
+    )
+
+    try {
+        New-Item "$Path\$Name" -ItemType File -ErrorAction Stop
+    }
+    catch [System.IO.DirectoryNotFoundException] {
+        New-Item $Path -ItemType Directory -Force
+        New-LogFile
+    }
+    catch {
+        "Some error other than DirectoryNotFound"
+    }
+}
