b14000fc8efeb353384b68076a396d60abbe11a0
Ram Iyer <ramakrishnan.rkology@gmail.com>
2018-10-24 15:58:46 -0500

    Add script to redirect verbose output

---
+++ ch14/08-New-LogFile.ps1
@@ -0,0 +1,30 @@
+function New-LogFile {
+    param (
+        # The path to the log file
+        [Parameter(Mandatory=$false)]
+        [string]
+        $Path = "$HOME/random/LogDir",
+
+        # The name of the log file
+        [Parameter(Mandatory=$false)]
+        [string]
+        $Name = 'MyLog.log'
+    )
+
+    try {
+        Write-Verbose "Creating the file, $Name at $Path."
+        New-Item "$Path/$Name" -ItemType File -ErrorAction Stop
+    }
+    catch [System.IO.DirectoryNotFoundException] {
+        Write-Verbose "System.IO.DirectoryNotFoundException encountered."
+        Write-Verbose "Creating the directory, $Path."
+        New-Item $Path -ItemType Directory -Force
+        Write-Verbose "Calling the function."
+        New-LogFile
+    }
+    catch {
+        Write-Verbose "The code tag."
+        "Some error other than DirectoryNotFound"
+    }
+}
+New-LogFile -Verbose 4>> verbose.log
