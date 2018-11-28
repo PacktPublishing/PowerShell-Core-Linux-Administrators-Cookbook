1e5d1c01c4167ccc0d338ada507f31048e11d9b3
Ram Iyer <ramakrishnan.rkology@gmail.com>
2018-11-28 11:24:36 +0530

    Fix 10.06

--- ch10/06-New-FilesAndDirectories.ps1
+++ ch10/06-New-FilesAndDirectories.ps1
@@ -1,8 +1,8 @@
 Set-Location (Join-Path $HOME random)
 
-Join-Path dir-01, dir-02, dir-03, dir-04 -ChildPath demo.txt | ForEach-Object { New-Item $PSItem -ItemType File -WhatIf }
+Join-Path dir-01, dir-02, dir-03, dir-04 -ChildPath demo.txt | ForEach-Object { New-Item $PSItem -ItemType File }
 
-'random-text.txt', 'himalayas.jpg', 'crunched-numbers.csv', 'screenshot-001.png', 'screenshot-002.png', 'screenshot-003.png', 'demo.doc', 'my-plugin.rb' | ForEach-Object { New-Item (Join-Path dir-01 -ChildPath $PSItem) -ItemType File -WhatIf }
+'random-text.txt', 'himalayas.jpg', 'crunched-numbers.csv', 'screenshot-001.png', 'screenshot-002.png', 'screenshot-003.png', 'demo.doc', 'my-plugin.rb' | ForEach-Object { New-Item (Join-Path dir-01 -ChildPath $PSItem) -ItemType File }
 
 Rename-Item screenshot-001.png myscreenshot.png
 # Verbose version: Rename-Item -Path screenshot-001.png -NewName myscreenshot.png
